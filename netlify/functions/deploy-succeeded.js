e363fda4135061f947a554863e208a7d49705ed1
Steven Trask <steve@gosupernova.co.uk>
2021-06-02 00:31:22 +0100

    test2

--- netlify/functions/deploy-succeeded.js
+++ netlify/functions/deploy-succeeded.js
@@ -2,7 +2,7 @@ const algoliasearch = require('algoliasearch');
 
 exports.handler = (event, context, callback) => {
   // Here we'll use Algolia to get upload
-  var client = algoliasearch('LY61NP6ZYV', '553fbe19fdd15da818acb34b3674973f');
+  var client = algoliasearch('LY61NP6ZYV', 'XXXX');
 	var index = client.initIndex('dev_f2');
 	var contentJSON = require('asset/content.json');
 
