9742e822ef9f93597d5cf66a4437f59693bc5288
Steven Trask <steve@gosupernova.co.uk>
2021-06-02 00:25:22 +0100

    test

--- netlify/functions/deploy-succeeded.js
+++ netlify/functions/deploy-succeeded.js
@@ -1,7 +1,15 @@
-exports.handler = async function(event, context) {
-  // your server-side functionality
-  return {
-      statusCode: 200,
-      body: JSON.stringify({message: "Hello World"})
-  };
-}
+const algoliasearch = require('algoliasearch');
+
+exports.handler = (event, context, callback) => {
+  // Here we'll use Algolia to get upload
+  var client = algoliasearch('LY61NP6ZYV', '553fbe19fdd15da818acb34b3674973f');
+	var index = client.initIndex('dev_f2');
+	var contentJSON = require('asset/content.json');
+
+	index.addObjects(contentJSON, function(err, content) {
+	  if (err) {
+	    console.error(err);
+	    callback(err);
+	  }
+	});
+};
